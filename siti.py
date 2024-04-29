fa07909d59ec34d779e71ce810a21e3562b76b45
Henrique Domingues Garcia <39018713+henriquedgarcia@users.noreply.github.com>
2024-04-29 16:15:45 -0300

    Update siti.py
    change skvideo to opencv
    fix params
    fix help
    fix csv
    use pathlib
    rename some variables
    remove legacy code
    remove comments
    remove old docstrings

--- siti.py
+++ siti.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 import argparse
+from collections import defaultdict
+from pathlib import Path
 
+import cv2
 import numpy as np
 import pandas as pd
-import skvideo.io
 from scipy import ndimage
-import os
 
 
 class SiTi:
     """
-    Calcule the Spatial information and Temporal information according
+    Calculate the Spatial information and Temporal information according
     ITU-T P.910, but using median instead of the maximum value. In the
     recommendation, the SI/TI for the video is the maximum value. But, for long
     videos the values vary a lot. In this way, the median will represent that
@@ -36,67 +37,86 @@ class SiTi:
 
     TI = median_time(std_space(difference(Frame_n, Frame_n-1)))
 
-
     """
 
-    def __init__(self, filename, scale, format=None, pix_fmt=None):
-        """
-
-        :param filename: The filename of video in path-like format.
-        :param scale: The frame dimension in string format. e.g.: "640x480".
-        :param format: force ffmpeg format. E.g.: "yuv", "mp4", "hevc", etc. See
-        "ffmpeg -formats".
-        :param pix_fmt: force ffmpeg pixel format. E.g.: "yuv420p", "yuv444p",
-        "rgb8", etc.
-        """
-        self.filename = filename
-        self.scale = scale
-        self.width, self.height = tuple(map(int, scale.split('x')))
+    def __init__(self, video: str, output: str, n_frames: int = None, verbose=False):
+        self.video = Path(video)
+        self.output = Path(output)
+        self.n_frames = n_frames
+        self.verbose = verbose
 
         self.si = []
         self.ti = []
         self.previous_frame = None
         self.frame_counter = 0
 
-        self.inputdict = {'-s': f'{self.scale}'}
+        self.calc_siti()
+        self.save_siti()
+        self.save_stats()
 
-        if format:
-            self.inputdict["-f"] = form
-        if pix_fmt:
-            self.inputdict["-pix_fmt"] = pix_fmt
+    def calc_siti(self):
+        """
+        Start the calculation of the SI and TI. The values are stored in the 'si'
+        and 'ti' attributes for each frame.
+
+        :return: None
+        """
+
+        for self.frame_counter, frame in enumerate(self._iter_video(), 1):
+            if self.frame_counter > self.n_frames:
+                break
+
+            value_si, sobel = self._calc_si(frame)
+            value_ti, difference = self._calc_ti(frame)
+
+            if self.verbose:
+                print(f"{self.frame_counter:04}, "
+                      f"si={value_si:05.3f}, ti={value_ti:05.3f}")
+            else:
+                print('.', end='', flush=True)
+
+    def _iter_video(self):
+        cap = cv2.VideoCapture(f'{self.video}')
+        while True:
+            ok, frame = cap.read()
+            if not ok:
+                break
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            yield gray.astype(float)
 
     @staticmethod
-    def sobel(frame):
+    def _sobel(frame):
         """
         Apply 1st order 2D Sobel filter
         :param frame:
         :return:
         """
-        sobx = ndimage.sobel(frame, axis=0)
-        soby = ndimage.sobel(frame, axis=1)
-        sob = np.hypot(sobx, soby)
+        sob_x = ndimage.sobel(frame, axis=0)
+        sob_y = ndimage.sobel(frame, axis=1)
+        sob = np.hypot(sob_x, sob_y)
         return sob
 
     def _calc_si(self, frame: np.ndarray) -> (float, np.ndarray):
         """
-        Calcule Spatial Information for a video frame.
+        Calculate Spatial Information for a video frame.
         :param frame: A luma video frame in numpy ndarray format.
         :return: spatial information and sobel frame.
         """
-        sobel = self.sobel(frame)
+        sobel = self._sobel(frame)
+        # cv2.imshow('frame', sobel)
         si = sobel.std()
         self.si.append(si)
         return si, sobel
 
     def _calc_ti(self, frame: np.ndarray) -> (float, np.ndarray):
         """
-        Calcule Temporal Information for a video frame. If is a first frame,
+        Calculate Temporal Information for a video frame. If is a first frame,
         the information is zero.
         :param frame: A luma video frame in numpy ndarray format.
-        :return: Temporal information and diference frame. If first frame the
-        diference is zero array on same shape of frame.
+        :return: Temporal information and difference frame. If first frame the
+        difference is zero array on same shape of frame.
         """
-        if self.previous_frame:
+        if self.previous_frame is not None:
             difference = frame - self.previous_frame
             ti = difference.std()
         else:
@@ -108,91 +128,64 @@ class SiTi:
 
         return ti, difference
 
-    def calc_siti(self, verbose=False, n_frames=None):
-        """
-        Start the calculation of the SI and TI. The values is stored in the 'si'
-        and 'ti' attributes for each frame.
-
-        :param verbose: Show calculation frame-by-frame.
-        :param n_frames: Frames to be processed.
-
-        :return: None
-        """
-
-        vreader = skvideo.io.vreader(fname=self.filename, as_grey=True,
-                                     inputdict=self.inputdict)
-
-        for self.frame_counter, frame in enumerate(vreader, 1):
-            if self.frame_counter > n_frames:
-                break
-            width = frame.shape[1]
-            height = frame.shape[2]
-            frame = frame.reshape((width, height)).astype('float32')
-            value_si, sobel = self._calc_si(frame)
-            value_ti, difference = self._calc_ti(frame)
-            if verbose:
-                print(f"{self.frame_counter:04}, "
-                      f"si={value_si:05.3f}, ti={value_ti:05.3f}")
-            else:
-                print('.', end='', flush=True)
-
-    def save_siti(self, filename=None):
-        if not filename:
-            filename, ext = os.path.splitext(self.filename)
-            filename = filename + '.csv'
+    def save_siti(self):
+        filename = self.video.with_suffix('.csv')
 
         df = pd.DataFrame({'si': self.si, 'ti': self.ti},
                           index=range(len(self.si)))
-        df.to_csv(f'{filename}.csv', index_label='frame')
+        df.to_csv(f'{filename}', index_label='frame')
 
-    def save_stats(self, filename=None):
-        if not filename:
-            filename, ext = os.path.splitext(self.filename)
-            filename = filename + '.csv'
+    def save_stats(self):
+        stats = defaultdict(list)
+        stats['si_average'].append(f'{np.average(self.si):05.3f}')
+        stats['si_std'].append(f'{np.std(self.si):05.3f}')
+        stats['si_0q'].append(f'{np.quantile(self.si, 0.00):05.3f}')
+        stats['si_1q'].append(f'{np.quantile(self.si, 0.25):05.3f}')
+        stats['si_2q'].append(f'{np.quantile(self.si, 0.50):05.3f}')
+        stats['si_3q'].append(f'{np.quantile(self.si, 0.75):05.3f}')
+        stats['si_4q'].append(f'{np.quantile(self.si, 1.00):05.3f}')
 
-        stats = dict(si_average=f'{np.average(self.si):05.3f}',
-                     si_std=f'{np.std(self.si):05.3f}',
-                     si_0q=f'{np.quantile(self.si, 0.00):05.3f}',
-                     si_1q=f'{np.quantile(self.si, 0.25):05.3f}',
-                     si_2q=f'{np.quantile(self.si, 0.50):05.3f}',
-                     si_3q=f'{np.quantile(self.si, 0.75):05.3f}',
-                     si_4q=f'{np.quantile(self.si, 1.00):05.3f}',
-                     ti_average=f'{np.average(self.ti):05.3f}',
-                     ti_std=f'{np.std(self.ti):05.3f}',
-                     ti_0q=f'{np.quantile(self.ti, 0.00):05.3f}',
-                     ti_1q=f'{np.quantile(self.ti, 0.25):05.3f}',
-                     ti_2q=f'{np.quantile(self.ti, 0.50):05.3f}',
-                     ti_3q=f'{np.quantile(self.ti, 0.75):05.3f}',
-                     ti_4q=f'{np.quantile(self.ti, 1.00):05.3f}')
+        stats['ti_average'].append(f'{np.average(self.ti):05.3f}')
+        stats['ti_std'].append(f'{np.std(self.ti):05.3f}')
+        stats['ti_0q'].append(f'{np.quantile(self.ti, 0.00):05.3f}')
+        stats['ti_1q'].append(f'{np.quantile(self.ti, 0.25):05.3f}')
+        stats['ti_2q'].append(f'{np.quantile(self.ti, 0.50):05.3f}')
+        stats['ti_3q'].append(f'{np.quantile(self.ti, 0.75):05.3f}')
+        stats['ti_4q'].append(f'{np.quantile(self.ti, 1.00):05.3f}')
 
-        name = os.path.basename(filename)
-        df = pd.DataFrame({name: stats}, index=range(len(self.si)))
-        df.to_csv(f'{filename}', index_label='Stats')
+        pd.DataFrame(stats).to_csv(f'{self.output}', index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='Calculate SI/TI according ITU-T P.910, but using median instead of the maximum value.',
+        prog='SiTi',
+        description='Calculate SI/TI for all frames according ITU-T P.910, and creates average, '
+                    'standard deviation, maximum, minimum and quartile statistics.'
+                    ' '
+                    'The file must be in a container format, such as MP4, AVI, MKV, etc., '
+                    'for OpenCV to read the metadata. Convert the file using the x264 '
+                    'encoder without FFmpeg with the CRF parameter set to 0. For example:'
+                    ' '
+                    'ffmpeg -video_size 960x480 -framerate 25 -pixel_format yuv420p -format '
+                    'rawvideo -i input_video.yuv -crf 0 output_video.mp4'
+                    ' '
+                    'See: https://trac.ffmpeg.org/wiki/Encode/H.264, and https://ffmpeg.org/ffmpeg.html',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('video', type=str, help="Video to analyze")
-    parser.add_argument('--num_frames', type=int, default=0,
-                        help='Process number of frames')
-    parser.add_argument('--output', type=str, default="",
-                        help="Output CSV for si ti report and statistics")
-    parser.add_argument('--size', type=str, default='0x0',
-                        help='Dimensions in pixels')
-    parser.add_argument('--form', type=str, default="",
-                        help="Force ffmpeg video format")
-    parser.add_argument("--pix_fmt", type=str, default="yuv420p",
-                        help="force ffmpeg pixel format")
+    parser.add_argument('-o', '--output',
+                        type=str,
+                        default='output.csv',
+                        help='Output CSV for si ti report and statistics. Default: output.csv.')
+    parser.add_argument('-n', '--n_frames',
+                        type=int,
+                        default=None,
+                        help='Process n frames.')
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        help="Show message in terminal.")
+    parser.add_argument('video',
+                        type=str,
+                        help="Video input to analyze.")
+
     params = vars(parser.parse_args())
 
-    video = params['video']
-    size = params['size']
-    num_frames = params['num_frames']
-    output = params['output']
-    form = params['form']
-    pix_fmt = params['pix_fmt']
-
-    siti = SiTi(video, size)
-    siti.calc_siti()
+    SiTi(**params)
